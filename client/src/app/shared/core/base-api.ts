63b7198c105aab5950b09752927d5ce12d6bb930
kepamuk <kepamuk@yandex.ru>
2018-03-22 17:10:48 +0300

    change port

--- client/src/app/shared/core/base-api.ts
+++ client/src/app/shared/core/base-api.ts
@@ -6,7 +6,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class BaseApi {
 
-    private url = 'http://localhost:8080/';
+    // private url = 'http://localhost:8080/';
+    private url = 'http://kepamuuw.beget.tech/';
 
     constructor(public http: HttpClient) {
     }
